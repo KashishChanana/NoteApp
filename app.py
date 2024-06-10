676baaf9ae2d6682718d6f02bef039f9ae3d3638
Kashish Chanana <chananakashish1998@gmail.com>
2024-06-09 20:48:45 -0700

    Add Sidebar for file upload

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ from main import Workflow
 
 def main():
     st.title("NoteApp")
-    input_url, input_model, input_task = st.columns(3)
+    # input_url, input_model, input_task = st.sidebar.columns(3)
 
-    with input_url:
+    youtube_sidebar, file_sidebar = st.sidebar.tabs(["YouTube", "File Upload"])
+    with youtube_sidebar:
         input_url = st.text_input("YouTube URL")
 
-    with input_model:
-        model = st.selectbox("Generative Model", ["GPT-3.5"])
+    with file_sidebar:
+        input_file = st.file_uploader("Choose a PDF")
 
-    with input_task:
-        task = st.selectbox("Generative Task", ["Blog", "Summary"])
+    model = st.sidebar.selectbox("Generative Model", ["GPT-3.5"])
+    task = st.sidebar.selectbox("Generative Task", ["Blog", "Summary"])
+    generate_button = st.sidebar.button("Generate", type="secondary")
 
-    st.divider()
-
-    if input_url:
+    if generate_button and (input_url or input_file):
         st.video(input_url)
         response = Workflow().run(youtube_url=input_url, model=model, task=task)
         if response:
